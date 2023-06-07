7bc1c37699776164c1f7890eb9784b62967da1a0
Miquido Ops <mqtc@miquido.com>
2023-06-07 17:19:13 +0000

    chore(deps): bump cloudposse/alb/aws from 1.6.0 to 1.9.0
    Bumps [cloudposse/alb/aws](https://github.com/cloudposse/terraform-aws-alb) from 1.6.0 to 1.9.0.
    - [Release notes](https://github.com/cloudposse/terraform-aws-alb/releases)
    - [Commits](https://github.com/cloudposse/terraform-aws-alb/compare/1.6.0...1.9.0)

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "alb" {
   source  = "cloudposse/alb/aws"
-  version = "1.6.0"
+  version = "1.9.0"
 
   name                                    = var.name
   namespace                               = var.project
